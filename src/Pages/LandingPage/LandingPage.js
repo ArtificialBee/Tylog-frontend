12bc3ac8f0e9d87a123f18e95fa0fd05428c22e9
ArtificialBee <edvinsacic@outlook.com>
2021-11-08 22:08:16 +0100

    Basic style for web app

---
+++ src/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LandingPageStyle = styled.div`
+    width:100vw;
+    height:100vh;
+`;
+
+const LandingPage = () => {
+    return (
+        <div>
+            This is the landing page!
+        </div>
+    );
+}
+
+export default LandingPage;
